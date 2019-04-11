d9a5001ac73166d69ea0d680da262d2f17c41279
Spencer Nauman <snauman817@gmail.com>
2019-04-11 12:18:04 -0400

    Writes tests for home route making sure users are redirected if not logged in

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,9 +12,12 @@ def test_index(client):
 
 def test_home(client, auth):
     response = client.get('/home')
-    assert b'<h1>TSCT Portal</h1>' in response.data
+    assert response.status_code == 302
 
     user = auth.login_teacher()
     response = client.get('/home')
-    
+    assert b'<h1>Welcome to the Portal</h1>' in response.data
+
+    usr = auth.login_student()
+    response = client.get('/home')
     assert b'<h1>Welcome to the Portal</h1>' in response.data
