5ccdc7d485179ea45c2dda260fa4bc016fca12ee
Spencer Nauman <snauman817@gmail.com>
2019-04-11 14:02:49 -0400

    Adds tables and test data into database
    - Creates courses and user_courses tables
    - Inserts test data into courses and user_courses

--- portal/schema.sql
+++ portal/schema.sql
@@ -1,8 +1,27 @@
+DROP TABLE IF EXISTS user_courses;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS courses;
 
 CREATE TABLE users (
     id bigserial PRIMARY KEY,
     email text UNIQUE NOT NULL,
     password text NOT NULL,
     role varchar(7) NOT NULL CHECK (role IN ('teacher', 'student'))
+);
+
+CREATE TABLE courses (
+    id bigserial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    course_code varchar(50) NOT NULL,
+    day varchar(7) NOT NULL, -- M T W R F S U
+    start_time time NOT NULL,
+    end_time time NOT NULL,
+    session varchar(2),
+    description text NOT NULL
+);
+
+CREATE TABLE user_courses (
+    id bigserial PRIMARY KEY,
+    user_id bigint REFERENCES users (id) NOT NULL,
+    course_id bigint REFERENCES courses (id) NOT NULL
 );
--- tests/data.sql
+++ tests/data.sql
@@ -4,3 +4,12 @@ INSERT INTO users (email, password, role)
 VALUES ('teacher@stevenscollege.edu', 'qwerty', 'teacher'),
        ('student@stevenscollege.edu', 'asdfgh', 'student');
 
+INSERT INTO courses (name, course_code, day, start_time, end_time, session, description)
+VALUES ('Course 1', 'CSET 180', 'MTWRF', '12:00:00', '16:30:00', 'A', 'Test Course'),
+       ('Course 2', 'CSET 170', 'MWF', '8:00:00', '9:50:00', 'C', 'Test Course Number 2');
+
+INSERT INTO user_courses (user_id, course_id)
+VALUES (1, 1),
+       (2, 1),
+       (1, 2),
+       (2, 2);
