18ca6d4a2dc94cf01a59fda8bfddc79e4176d130
Spencer Nauman <snauman817@gmail.com>
2019-04-11 12:49:33 -0400

    Tests login errors on index route

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,7 +5,9 @@ def test_login(client, auth):
     print(response.headers)
     print(response.data)
 
-    # with client:
-    #     client.get('/')
-    #     assert session['id'] == 1
-    #     assert g.user['email'] == 'teacher@stevenscollege.edu'
+def test_flash_error(client, auth):
+    response = client.post('/', data=dict(email='fake@email.com', password='fake'))
+    assert b'<li class="message">Incorrect email</li>' in response.data
+
+    response = client.post('/', data=dict(email='teacher@stevenscollege.edu', password='fake'))
+    assert b'<li class="message">Incorrect password</li>' in response.data
