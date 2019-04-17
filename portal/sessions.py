8b66ef43c6aef2b2aed06bca5df5a08297f7bdad
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-17 15:45:05 -0400

    make route and view to add session

--- portal/sessions.py
+++ portal/sessions.py
@@ -5,8 +5,23 @@ from .auth import login_required
 
 bp = Blueprint('sessions', __name__)
 
-@bp.route('/add-session')
+@bp.route('/sessions/add/<int:course_id>')
 @login_required
-def add_session():
-    return 'this is the session bp'
+def add_session(course_id):
+    con = db.get_db()
+    cur = con.cursor() 
+
+    cur.execute("""
+        INSERT INTO sessions (course_id, session_name, day, start_time, end_time)
+        VALUES (%s, %s, %s, %s, %s)
+    """,
+    (course_id, 'A', 'M', '12:00:00', "1:00:00"))
+
+    con.commit()
+
+    cur.close()
+    con.close()
+
+    return redirect(url_for('courses.edit_courses', id=course_id))
+    
 
