e52d080d5b1d457ee862c7b77cf9a303bd6676d6
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-23 12:06:58 -0400

    add students to sessions

--- portal/sessions.py
+++ portal/sessions.py
@@ -38,6 +38,70 @@ def add_session(course_id):
 
         else: 
             return render_template('add_session.html')
+
+@bp.route('/sessions/add_student/<int:session_id>')
+@login_required
+def add_student(session_id):
+    if g.user[3] != 'teacher':
+        return make_response("Unauthorized", 401)
+    elif g.user[3] == 'teacher':
+
+        con = db.get_db()
+        cur = con.cursor() 
+
+        cur.execute("""
+            SELECT id, email FROM users
+            WHERE role = 'student';
+        """)
+
+        students = cur.fetchall()
+        # TODO select all users sessions where session id = current session id
+        cur.execute("""
+            SELECT users.id, users.email, user_sessions.session_id FROM user_sessions
+            JOIN users ON user_sessions.user_id = users.id
+            WHERE user_sessions.session_id = %s
+            AND users.role = 'student';
+
+        """, (session_id,))
+
+        added_students = cur.fetchall()
+
+        cur.close()
+        con.close()
+
+       # unadded_students = somefunc(students, added_students) 
+
+        x = students + added_students
+        y = set(x)
+        print(y)
+
+
+
+        return render_template('add_student.html', session_id=session_id, unadded_students=y, added_students=added_students)
+
+@bp.route('/sessions/add_student/<int:session_id>/new/<int:user_id>')
+@login_required
+def add_new_student(session_id, user_id, methods=['GET']):
+    if g.user[3] != 'teacher':
+        return make_response("Unauthorized", 401)
+    elif g.user[3] == 'teacher':
+        con = db.get_db()
+        cur = con.cursor() 
+
+        cur.execute(""" 
+            INSERT INTO user_sessions (user_id, session_id)
+            VALUES (%s, %s) """, (user_id, session_id))    
+
+        con.commit()
+
+        cur.close()
+        con.close()
+
+        return redirect(url_for('sessions.add_student', session_id=session_id))    
+
+
+
+
         
 
 
