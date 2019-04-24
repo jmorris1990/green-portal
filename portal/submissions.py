5ab7ae8102af1b7f4f6843f057270c76de01100d
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-24 15:42:29 -0400

    create db queries for submissions

--- portal/submissions.py
+++ portal/submissions.py
@@ -12,8 +12,23 @@ def view_assignments(assignment_id):
     if g.user != 'teacher':
         return make_response("Unauthorized", 401)
     else:
-        con.db.get_db()
+        con = db.get_db()
         cur = con.cursor()
+
+        cur.execute(""" 
+            SELECT session_id FROM assignments
+            WHERE id = %s
+            """,(assignment_id,))
+
+        session_id = cur.fetchone()
+
+        cur.execute("""
+            SELECT user_id FROM user_sessions
+            WHERE session_id = %s; 
+            AND role = 'student' """
+            ,(session_id,))
+
+        student_id = cur.fetchall()
 
         cur.execute("""
                     SELECT content, points_earned FROM submissions
@@ -23,6 +38,7 @@ def view_assignments(assignment_id):
                     (assignment_id, student_id))
 
         submission_list = cur.fetchall()
+        print(submission_list)
 
         cur.close()
         con.close()
