8b9e2fb0a3787209573db90161616f747df2cea5
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-24 12:53:44 -0400

    change route, fix sessions.html css and link

--- portal/sessions.py
+++ portal/sessions.py
@@ -26,15 +26,16 @@ def sessions():
 
     return render_template('sessions.html', sessions_list=sessions_list)
 
-@bp.route('/sessions/add/<int:course_id>', methods=['GET', 'POST'])
+@bp.route('/sessions/add/', methods=['GET', 'POST'])
 @login_required    
-def add_session(course_id):
+def add_session():
     
     if g.user[3] != 'teacher':
         return make_response("Unauthorized", 401)
     elif g.user[3] == 'teacher':
         if request.method == 'POST':
-
+            course_id = None
+            
             session_name = request.form.get('session_name')
             day = request.form.get('day')
             start_time = request.form.get('start_time')
@@ -80,73 +81,7 @@ def add_session(course_id):
         else: 
             return render_template('add_session.html')
 
-@bp.route('/sessions/add_student/<int:session_id>')
-@login_required
-def add_student(session_id):
-    if g.user[3] != 'teacher':
-        return make_response("Unauthorized", 401)
-    elif g.user[3] == 'teacher':
 
-        con = db.get_db()
-        cur = con.cursor() 
-
-        cur.execute("""
-            SELECT id, email FROM users
-            WHERE role = 'student';
-        """)
-
-        students = cur.fetchall()
-        # TODO select all users sessions where session id = current session id
-        cur.execute("""
-            SELECT users.id, users.email FROM user_sessions
-            JOIN users ON user_sessions.user_id = users.id
-            WHERE user_sessions.session_id = %s
-            AND users.role = 'student';
-
-        """, (session_id,))
-
-        added_students = cur.fetchall()
-
-        cur.close()
-        con.close()
-
-
-        def filter_unadded(students, added_students):
-            unadded_students = []
-            for student in students:
-                if student not in added_students:
-                    unadded_students.append(student)
-            return unadded_students
-
-        unadded_students = filter_unadded(students, added_students) 
-
-
-        return render_template('add_student.html', session_id=session_id, unadded_students=unadded_students, added_students=added_students)
-
-@bp.route('/sessions/add_student/<int:session_id>/new/<int:user_id>')
-@login_required
-def add_new_student(session_id, user_id, methods=['GET']):
-    if g.user[3] != 'teacher':
-        return make_response("Unauthorized", 401)
-    elif g.user[3] == 'teacher':
-        con = db.get_db()
-        cur = con.cursor() 
-
-        # cur.execute(""" 
-        #     SELECT user_id, session_id FROM user_sessions 
-        #     WHERE session_id = %s
-        #     """, ())
-
-        cur.execute(""" 
-            INSERT INTO user_sessions (user_id, session_id)
-            VALUES (%s, %s) """, (user_id, session_id))    
-
-        con.commit()
-
-        cur.close()
-        con.close()
-
-        return redirect(url_for('sessions.add_student', session_id=session_id))    
 
 
 
