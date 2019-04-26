006aa01a1af63933c11d072e0e122561ffdc030f
Tyler Fink <tsfink182@stevenscollege.edu>
2019-04-26 15:25:51 -0400

    Fixed tests

--- tests/test_assignments.py
+++ tests/test_assignments.py
@@ -13,10 +13,19 @@ def test_create_assignments(client, auth):
     response = client.get('sessions/1/assignments/create')
     assert b'<form method="POST"' in response.data
 
+
+    response = client.post('sessions/0/assignments/create', data=dict(name='New Assignment', description='This is a new assignment.', total_points='100'))
+    assert b'<form method="POST"' in response.data
+
     response = client.post('sessions/1/assignments/create', data=dict(name='New Assignment', description='This is a new assignment.', total_points='100'))
     assert response.status_code == 302
+
     response = client.get('sessions/1/assignments')
     assert b'<ul' in response.data
     assert b'<li>' in response.data
     assert b'<span>New Assignment</span>' in response.data
     assert b'<p>This is a new assignment.</p>' in response.data
+
+    user = auth.login_student()
+    response = client.get('sessions/1/assignments/create')
+    assert response.status_code == 401
