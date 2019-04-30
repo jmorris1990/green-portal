fde5808e23991312acc494699bb2eff339689e6b
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-30 15:59:39 -0400

    require login and check user role in uploads

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request, redirect, flash, url_for, send_from_directory
+from flask import Flask, render_template, g, request, redirect, make_response, flash, url_for, send_from_directory
 from .auth import login_required
 
 from werkzeug.utils import secure_filename
@@ -32,25 +32,30 @@ def create_app(test_config=None):
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     @app.route('/upload', methods=['GET', 'POST'])
+    @login_required
     def upload():
-        if request.method == 'POST':
-                # check if the post request has the file part
-                if 'file' not in request.files:
-                    flash('No file part')
-                    return redirect(request.url)
-                file = request.files['file']
-                # if user does not select file, browser also
-                # submit an empty part without filename
-                if file.filename == '':
-                    flash('No selected file')
-                    return redirect(request.url)
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    message = '{} uploaded'.format(filename)
-                    flash(message)
-                    return redirect(url_for('upload'))
-        return render_template('upload.html')
+        if g.user[3] != 'student':
+            return make_response("Unauthorized", 401)
+        else:
+
+            if request.method == 'POST':
+                    # check if the post request has the file part
+                    if 'file' not in request.files:
+                        flash('No file part')
+                        return redirect(request.url)
+                    file = request.files['file']
+                    # if user does not select file, browser also
+                    # submit an empty part without filename
+                    if file.filename == '':
+                        flash('No selected file')
+                        return redirect(request.url)
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                        message = '{} uploaded'.format(filename)
+                        flash(message)
+                        return redirect(url_for('upload'))
+            return render_template('upload.html')
 
     
     from . import db
