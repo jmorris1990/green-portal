e590cf02be867e3a86b8c4097cf118f54c3076bb
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-04-30 15:56:14 -0400

    Revert "broken"
    This reverts commit 47e6a95bb3fc2c92d7c7e0cb792b82da448ce4b1.

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,9 +1,11 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request, redirect, flash, url_for, send_from_directory
 from .auth import login_required
+
+from werkzeug.utils import secure_filename
 
 from . import db
 from .auth import login_required
+import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -22,6 +24,33 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
     
+
+    def allowed_file(filename):
+        ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload():
+        if request.method == 'POST':
+                # check if the post request has the file part
+                if 'file' not in request.files:
+                    flash('No file part')
+                    return redirect(request.url)
+                file = request.files['file']
+                # if user does not select file, browser also
+                # submit an empty part without filename
+                if file.filename == '':
+                    flash('No selected file')
+                    return redirect(request.url)
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    message = '{} uploaded'.format(filename)
+                    flash(message)
+                    return redirect(url_for('upload'))
+        return render_template('upload.html')
 
     
     from . import db
--- portal/uploads.py
+++ portal/uploads.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, flash, url_for, redirect, request, session, g, Blueprint, current_app, make_response
+from flask import render_template, flash, session, url_for, redirect, request, g, Blueprint
 from werkzeug.utils import secure_filename
 
 from . import db
@@ -7,39 +7,3 @@ from .auth import login_required
 
 bp = Blueprint('uploads', __name__)
 
-def allowed_file(filename):
-    ALLOWED_EXTENSIONS = set(['.doc', '.docx', '.pdf', '.txt'])
-
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@bp.route('/upload', methods=['GET', 'POST'])
-@login_required
-def upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(url_for('uploads.upload'))
-        file = request.files['file']
-        print(request.files)
-        print(file.filename)
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('Please select a file to upload.')
-            return redirect(url_for('uploads.upload'))
-        
-        if file and allowed_file(file.filename):
-            print("-----------------------------")
-            filename = secure_filename(file.filename)
-            print(filename)
-            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-            message = '{} uploaded'.format(filename)
-            print("-----------------------------")
-            print(message)
-            flash(message)
-            return redirect(url_for('uploads.upload'))
-    else:
-        return render_template('upload.html')
