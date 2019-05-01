acbaf6d1017f7685ef5cc3ae6dd4df6c65b8f73c
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2019-05-01 12:27:45 -0400

    plan out next steps and import required modules

--- portal/uploads.py
+++ portal/uploads.py
@@ -1,25 +1,28 @@
 import os
 from flask import render_template, flash, session, url_for, redirect, request, g, Blueprint, current_app
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
+
 
 from . import db
 from .auth import login_required
 
 bp = Blueprint('uploads', __name__)
 
+# prevent relative path traversal exploit eg ../../../malicious.txt
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@bp.route('/upload', methods=['GET', 'POST'])
+@bp.route('/upload', methods=['GET', 'POST']) #TODO: change route name and add session id param
 @login_required
 def upload():
     if g.user[3] != 'student':
         return make_response("Unauthorized", 401)
     else:
-
+        #TODO: query db, get the student id from g.user[0], get the submission id and type
         if request.method == 'POST':
                 # check if the post request has the file part
                 if 'file' not in request.files:
@@ -33,6 +36,7 @@ def upload():
                     return redirect(url_for('uploads.upload'))
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
+                    #TODO: append the submission id to the filename, then hash the filename, then insert that filename into the file field in the submissions table
                     file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
                     message = '{} uploaded'.format(filename)
                     flash(message)
